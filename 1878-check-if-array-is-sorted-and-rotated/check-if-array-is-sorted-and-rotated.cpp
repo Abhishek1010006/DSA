fc71658088b264fd66610168904ba630fcb992c0
Abhishek Kumar Singh <abhishekkumarsngh000@gmail.com>
2026-07-11 17:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (82.21%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -8,6 +8,7 @@ public:
             }
         }
 
+        // This is done because array sorted hai last ke largest numbers hi aage aayenge 
         if(nums[nums.size()-1]>nums[0]) count++;
 
         return count<=1;
