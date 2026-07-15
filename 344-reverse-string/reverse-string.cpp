5c42d618ed25c4c82baceca14aa15350e6e4baca
Abhishek Kumar Singh <abhishekkumarsngh000@gmail.com>
2026-07-15 16:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (15.65%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         int j=s.size()-1;
         int i=0;
         while(i<j){
-            swap(s[i],s[j]);
-            i++;
-            j--;
+            swap(s[i++],s[j--]);
         }
     }
 };
